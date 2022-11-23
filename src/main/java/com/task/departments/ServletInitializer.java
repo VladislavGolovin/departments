d9341dbdf3da7b2ab5c455d5e60586a80c960f20
Vladislav Golovin <golovin.vladislav@gmail.com>
2022-11-23 18:20:38 +0300

    initial commit

---
+++ src/main/java/com/task/departments/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.task.departments;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DepartmentsApplication.class);
+	}
+
+}
